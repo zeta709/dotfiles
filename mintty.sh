12451f4fbfde42b7e5f9e21ed6fb76ac0eb8dc58
zeta709 <zeta709@gmail.com>
2017-07-22 16:14:23 +0900

    Set mintty cursor in vim

--- mintty.sh
+++ mintty.sh
@@ -3,5 +3,16 @@
 SELF_DIR="$( unset CDPATH && cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # TODO: prevent muliple installs
+echo "# solarized" >> ~/.minttyrc
 cat ${SELF_DIR}/mintty-colors-solarized/.minttyrc.dark >> ~/.minttyrc
+echo "# dot.mintty" >> ~/.minttyrc
 cat ${SELF_DIR}/mintty/dot.mintty >> ~/.minttyrc
+
+cat <<- 'EOF' >> ~/.vimrc
+	" mintty
+	let &t_ti.="\e[1 q"
+	let &t_SI.="\e[5 q"
+	let &t_EI.="\e[1 q"
+	let &t_te.="\e[0 q"
+
+EOF
