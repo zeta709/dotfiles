43cefc1fc096d11ead78f73be38d11bb51b8c382
zeta709 <zeta709@gmail.com>
2018-02-17 20:11:35 +0900

    dot.zsh: add suffix aliases; fix minor bugs

--- zsh/dot.zsh
+++ zsh/dot.zsh
@@ -1,16 +1,19 @@
 [[ -z "$DOTFILES" ]] && DOTFILES="$HOME/.dotfiles"
 
 load_oh-my-zsh() {
+	## TODO: remove oh-my-zsh
 	## ref: oh-my-zsh/templates/zshrc.zsh-template
 
-	local ZSH="$DOTFILES/zsh/oh-my-zsh"
+	# some variables should be non-local since they are used in funtions
+	ZSH="$DOTFILES/zsh/oh-my-zsh"
+
 	[[ -z "${ZSH_THEME+x}" ]] && local ZSH_THEME="gentoo2"
 	local HYPHEN_INSENSITIVE="true"
 	local DISABLE_AUTO_UPDATE="true"
-	local DISABLE_UNTRACKED_FILES_DIRTY="true"
+	DISABLE_UNTRACKED_FILES_DIRTY="true"
 	local HIST_STAMPS="yyyy-mm-dd"
-	local ZSH_CUSTOM="$DOTFILES/zsh/custom"
-	#local plugins=()
+	ZSH_CUSTOM="$DOTFILES/zsh/custom"
+	#plugins=()
 
 	source $ZSH/oh-my-zsh.sh
 }
@@ -19,7 +22,7 @@ load_oh-my-zsh && unset -f load_oh-my-zsh
 source "$DOTFILES/zsh/markdown.zsh"
 source "$DOTFILES/z/z.sh"
 
-[ -r "$DOTFILES/.dircolors" ] && eval `dircolors "$DOTFILES/.dircolors"`
+[[ -r "$DOTFILES/.dircolors" ]] && eval `dircolors "$DOTFILES/.dircolors"`
 
 export PATH="$HOME/bin:$PATH"
 export LANG=en_US.UTF-8
@@ -30,6 +33,12 @@ alias rm="rm -i"
 alias cp="cp -i"
 alias mv="mv -i"
 alias server="python3 -m http.server" # simple server
+
+fts=(c cc cpp h hpp md txt)
+for ft in $fts; do
+	alias -s $ft=$EDITOR;
+done
+unset fts
 
 #unsetopt autonamedirs
 unsetopt cdablevars
