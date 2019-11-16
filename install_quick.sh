d3490031e30c738bc6ffab0ae3f2012e9e41b764
zeta709 <zeta709@gmail.com>
2019-11-16 12:52:30 +0900

    Add install_quick.sh

---
+++ install_quick.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/zeta709/dotfiles/master/install.sh)"
+
+set -e
+
+cd $HOME
+git clone https://github.com/zeta709/dotfiles.git .dotfiles
+cd .dotfiles
+git submodule update --init --recursive
+./install.sh
+vim +PlugInstall +qall
+echo 1 | ./colorscheme.sh
