d6f246538c904c212348d411ee5c427143c35037
zeta709 <zeta709@gmail.com>
2022-02-04 22:36:38 +0900

    Update install.sh (style)

--- install.sh
+++ install.sh
@@ -1,63 +1,61 @@
 #!/bin/bash
+# vim: noexpandtab:sw=4:ts=4
 
-# usage: source_rc "$BASERC" "$RC"
+# usage: source_rc "$baserc_escaped" "$rc"
 source_rc() {
-	local BASERC="$1"
-	local RC="$2"
-	[ -f "$RC" ] || touch "$RC"
-	if ! grep -Fq "source $BASERC" "$RC"; then
-		echo "source $BASERC" >> "$RC"
+	local baserc="$1"
+	local rc="$2"
+	[ -f "$rc" ] || touch "$rc"
+	if ! grep -Fq "source $baserc" "$rc"; then
+		echo "source $baserc" >> "$rc"
 	fi
 }
 
-SELF_DIR="$( unset CDPATH && cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-SELF_DIRQ="$( printf "%q" "$SELF_DIR" )"
+scriptdir="$(unset CDPATH && cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+scriptdir_escaped="$(printf "%q" "$scriptdir")"
 
 # zsh
-source_rc "$SELF_DIRQ/zsh/dot.zsh" "$HOME/.zshrc"
+source_rc "$scriptdir_escaped/zsh/dot.zsh" "$HOME/.zshrc"
 echo "note: .zlogin/.zlogout will not be installed automatically"
 
 # vim
 [ -f "$HOME/.vimrc" ] || touch "$HOME/.vimrc"
 if [ ! -f "$HOME/.vim/autoload/plug.vim" ]; then
 	mkdir -p "$HOME/.vim/autoload"
-	ln -vis "$SELF_DIR/vim/vim-plug/plug.vim" "$HOME/.vim/autoload/plug.vim"
+	ln -vis "$scriptdir/vim/vim-plug/plug.vim" "$HOME/.vim/autoload/plug.vim"
 fi
-STR="\" dotfiles are installed"
-if ! grep -q "$STR" "$HOME/.vimrc"; then
+if ! grep -q "$scriptdir_escaped" "$HOME/.vimrc"; then
 	cat <<- EOF >> "$HOME/.vimrc"
-		source $SELF_DIRQ/vim/dot.vim
-		source $SELF_DIRQ/vim/gui.vim
+		source $scriptdir_escaped/vim/dot.vim
+		source $scriptdir_escaped/vim/gui.vim
 
 		call plug#begin('$HOME/.vim/plugged')
-		source $SELF_DIRQ/vim/dot-plugins.vim
-		Plug '$SELF_DIRQ/fzf'
+		source $scriptdir_escaped/vim/dot-plugins.vim
+		Plug '$scriptdir_escaped/fzf'
 		Plug 'junegunn/fzf.vim'
 		call plug#end()
-		$STR
-
 	EOF
 fi
 
 # gdb
-source_rc "$SELF_DIRQ/gdb/dot.gdb" "$HOME/.gdbinit"
+source_rc "$scriptdir_escaped/gdb/dot.gdb" "$HOME/.gdbinit"
 
 # git
-DOTGITCONFIG="$(git config --global --get-all include.path "^$SELF_DIR/git/dot.gitconfig$")"
-if [ -z "$DOTGITCONFIG" ]; then
-	git config --global --add include.path "$SELF_DIR/git/dot.gitconfig"
+dotgitconfig="$(git config --global --get-all include.path "^$scriptdir/git/dot.gitconfig$")"
+if [ -z "$dotgitconfig" ]; then
+	git config --global --add include.path "$scriptdir/git/dot.gitconfig"
 fi
 
 # tmux
-source_rc "$SELF_DIRQ/tmux/dot.tmux.conf" "$HOME/.tmux.conf"
+source_rc "$scriptdir_escaped/tmux/dot.tmux.conf" "$HOME/.tmux.conf"
 
 # bin
 mkdir -p "$HOME/bin"
-find "$SELF_DIR/bin" -type f -executable -exec ln -vis -t "$HOME/bin" "{}" \;
+find "$scriptdir/bin" -type f -executable -exec ln -vis -t "$HOME/bin" "{}" \;
 
 ## fzf
-"$SELF_DIR/fzf/install"
-[[ ! -e "$HOME/.fzf" ]] && ln -vis "$SELF_DIR/fzf" "$HOME/.fzf"
+"$scriptdir/fzf/install"
+[ ! -e "$HOME/.fzf" ] && ln -vis "$scriptdir/fzf" "$HOME/.fzf"
 
 # etc.
-#ln -vis "$SELF_DIR/mutt/.muttrc" "$HOME/.muttrc"
+#ln -vis "$scriptdir/mutt/.muttrc" "$HOME/.muttrc"
